d0884d3297f429995152347e3e07b9f5a0db7799
Michael Heap <m@michaelheap.com>
2022-01-27 15:07:03 +0000

    Export octokit.rest.repos.createOrUpdateFiles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const plugin = require("./create-or-update-files");
 
 module.exports = function(octokit) {
-  octokit.repos.createOrUpdateFiles = plugin.bind(null, octokit);
+  octokit.rest.repos.createOrUpdateFiles = plugin.bind(null, octokit);
 };
